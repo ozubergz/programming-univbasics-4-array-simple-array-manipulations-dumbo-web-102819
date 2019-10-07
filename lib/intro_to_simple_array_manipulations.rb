062f0cce7ae80e286f73f624094e4a0c117f34cd
James Min <jmin499@gmail.com>
2019-10-07 21:28:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,3 +22,10 @@ end
 def shift_with_args(array, num = 2)
   array.shift(num)
 end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert(element, 4)
